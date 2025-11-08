5b4126a086840a8a4d6e5e70c37a7bc84f076a07
Edoardo Giacomello <edoardo.giacomello@fht.org>
2025-11-08 11:03:59 +0100

    [Add] Radius checkpointing

--- src/eps_seg/training/callbacks.py
+++ src/eps_seg/training/callbacks.py
@@ -1,6 +1,7 @@
 import lightning as L
 from lightning.pytorch.callbacks import EarlyStopping
-
+import shutil
+import os
 
 class EarlyStoppingWithPatiencePropagation(EarlyStopping):
     """
@@ -44,16 +45,31 @@ class RadiusSchedulerCallback(L.Callback):
     """
         At the end of each epoch, if no improvement has been seen for radius_increment_patience epochs,
         increase the radius by 1, up to max_radius.
+
+        Args:
+            radius_increment_patience (int): Number of epochs with no improvement to wait before increasing radius
+            best_ckpt_path (str): If provided, copies this checkpoint to a new file with the current radius in the filename whenever the radius is increased.
     """
     
-    def __init__(self, radius_increment_patience: int):
+    def __init__(self, radius_increment_patience: int, best_ckpt_path: str = None):
         super().__init__()
         self.radius_increment_patience = radius_increment_patience
+        self.best_ckpt_path = best_ckpt_path
     
+    def _backup_checkpoint(self, pl_module):
+        if self.best_ckpt_path is not None:
+            
+            radius = pl_module.current_radius
+            new_ckpt_path = f"{os.path.splitext(self.best_ckpt_path)[0]}_radius{radius}.ckpt"
+            shutil.copyfile(self.best_ckpt_path, new_ckpt_path)
+            print(f"Radius increased to {radius}. Backed up best checkpoint to {new_ckpt_path}.")
+
     def on_validation_epoch_end(self, trainer, pl_module):
         super().on_validation_epoch_end(trainer, pl_module)
         if pl_module.current_training_mode == "semisupervised":
             if self.radius_increment_patience > 0 and pl_module.current_radius_patience >= self.radius_increment_patience:
+                self._backup_checkpoint(pl_module)
+                # Increase radius
                 pl_module.trainer.datamodule.increase_radius()
                 pl_module.current_radius = min(pl_module.current_radius + 1, pl_module.train_cfg.max_radius)
                 pl_module.current_radius_patience = 0
